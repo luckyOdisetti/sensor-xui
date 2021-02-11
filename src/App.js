cee14c86daa5ad9470ab64e633df9db801c34b1e
Lucky Odisetti <lucky.odisetti@bitmex.com>
2021-02-11 13:25:22 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Map from "./MapContainer";
+import Suspension from "./Suspension";
+import Lean from "./Lean";
+import LapTable from "./LapTable";
+import { LAPS_DATA as LAPS_MAP } from "./laps_data.js";
 
 function App() {
+  const [lap, setLap] = React.useState(null);
+  const onSelect = (timestamp) => {
+    setLap(LAPS_MAP[timestamp]);
+  };
+  const lapIndex = Object.keys(LAPS_MAP);
+type Jobs: Array<{
+  jobId: number,
+  title: string,
+  locations: Array<object>,
+  departments: Array<object>
+}>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid">
+      <div className="left">
+        <div className="lap-table">
+          <h3>Lap Table</h3>
+          <ul>
+            {lapIndex.map((timestamp, idx) => (
+              <li onClick={() => onSelect(timestamp)} key={idx}>{`Lap ${
+                idx + 1
+              } :: ${new Date(Number(timestamp)).toDateString()}`}</li>
+            ))}
+          </ul>
+        </div>
+        <Map currentLap={lap} />
+      </div>
+      <div className="right">
+        <Suspension currentLap={lap} />
+        <Lean currentLap={lap} />
+      </div>
     </div>
   );
 }
